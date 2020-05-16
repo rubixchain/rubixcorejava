f167af68060e2a143bc1f0f874c4174a918f7daa
Rubix <63278901+rubix-chain@users.noreply.github.com>
2020-05-16 18:35:43 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ IPFS_PATH=/home/$user/.ipfs1 ipfs config --json Experimental.Libp2pStreamMountin
 IPFS_PATH=/home/$user/.ipfs1 ipfs config --json Swarm.EnableAutoRelay true
 IPFS_PATH=/home/$user/.ipfs1 ipfs config Addresses.API /ip4/127.0.0.1/tcp/5001
 IPFS_PATH=/home/$user/.ipfs1 ipfs config Addresses.Gateway /ip4/127.0.0.1/tcp/8081
-IPFS_PATH=~/.ipfs1 ipfs config --json Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip6/::/tcp/4001"]'
+IPFS_PATH=/home/$user/.ipfs1 ipfs config --json Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip6/::/tcp/4001"]'
 
 IPFS_PATH=/home/$user/.ipfs2 ipfs init
 cp -R swarm.key /home/$user/.ipfs2
@@ -19,7 +19,7 @@ IPFS_PATH=/home/$user/.ipfs2 ipfs config --json Experimental.Libp2pStreamMountin
 IPFS_PATH=/home/$user/.ipfs2 ipfs config --json Swarm.EnableAutoRelay true
 IPFS_PATH=/home/$user/.ipfs2 ipfs config Addresses.API /ip4/127.0.0.1/tcp/5002
 IPFS_PATH=/home/$user/.ipfs2 ipfs config Addresses.Gateway /ip4/127.0.0.1/tcp/8082
-IPFS_PATH=~/.ipfs2 ipfs config --json Addresses.Swarm '["/ip4/0.0.0.0/tcp/4002", "/ip6/::/tcp/4002"]'
+IPFS_PATH=/home/$user/.ipfs2 ipfs config --json Addresses.Swarm '["/ip4/0.0.0.0/tcp/4002", "/ip6/::/tcp/4002"]'
 
 
 IPFS_PATH=/home/$user/.ipfs3 ipfs init
@@ -30,7 +30,7 @@ IPFS_PATH=/home/$user/.ipfs3 ipfs bootstrap add /ip4/183.82.0.114/tcp/4001/ipfs/
 IPFS_PATH=/home/$user/.ipfs3 ipfs config --json Experimental.Libp2pStreamMounting true
 IPFS_PATH=/home/$user/.ipfs3 ipfs config --json Swarm.EnableAutoRelay true
 IPFS_PATH=/home/$user/.ipfs3 ipfs config Addresses.API /ip4/127.0.0.1/tcp/5003
-IPFS_PATH=/home/$user/~/.ipfs3 ipfs config Addresses.Gateway /ip4/127.0.0.1/tcp/8083
-IPFS_PATH=~/.ipfs3 ipfs config --json Addresses.Swarm '["/ip4/0.0.0.0/tcp/4003", "/ip6/::/tcp/4003"]'
+IPFS_PATH=/home/$user/.ipfs3 ipfs config Addresses.Gateway /ip4/127.0.0.1/tcp/8083
+IPFS_PATH=/home/$user/.ipfs3 ipfs config --json Addresses.Swarm '["/ip4/0.0.0.0/tcp/4003", "/ip6/::/tcp/4003"]'
 
 
