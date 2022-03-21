e31df34cc98adf6823b26d52035753c41eaedf01
Kiran H <kiran.h@rubix.network>
2022-03-21 14:09:03 +0530

    added exception handling

--- src/com/rubix/Resources/APIHandler.java
+++ src/com/rubix/Resources/APIHandler.java
@@ -199,25 +199,30 @@ public class APIHandler {
         String qstFile = WALLET_DATA_PATH.concat("QuorumSignedTransactions.json");
         String mineFile = WALLET_DATA_PATH.concat("MinedCreditsHistory.json");
 
+        JSONObject returnObject = new JSONObject();
         File quorumFile = new File(qstFile);
         File minedFile = new File(mineFile);
 
         int spentCredits = 0;
         int unspentCredits = 0;
-        if (quorumFile.exists()) {
-            String qFile = readFile(qstFile);
-            JSONArray qArray = new JSONArray(qFile);
-            unspentCredits = qArray.length();
+        try {
+            if (quorumFile.exists()) {
+                String qFile = readFile(qstFile);
+                JSONArray qArray = new JSONArray(qFile);
+                unspentCredits = qArray.length();
+            }
+            if (minedFile.exists()) {
+                String mFile = readFile(mineFile);
+                JSONArray mArray = new JSONArray(mFile);
+                spentCredits = mArray.length();
+            }
+    
+            
+            returnObject.put("spentCredits", spentCredits);
+            returnObject.put("unspentCredits", unspentCredits);
+        } catch (JSONException e) {
+            //TODO: handle exception
         }
-        if (minedFile.exists()) {
-            String mFile = readFile(mineFile);
-            JSONArray mArray = new JSONArray(mFile);
-            spentCredits = mArray.length();
-        }
-
-        JSONObject returnObject = new JSONObject();
-        returnObject.put("spentCredits", spentCredits);
-        returnObject.put("unspentCredits", unspentCredits);
 
         return returnObject;
     }
