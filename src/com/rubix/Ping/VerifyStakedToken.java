c4b8eb9ed043c13fcd57f92dec087b19c3fe333c
Nidhin Mahesh A <nidhin.m@rubix.network>
2022-04-06 15:39:09 +0530

    4x level height check in staked token transfer
    Signed-off-by: Nidhin Mahesh A <nidhin.m@rubix.network>

--- src/com/rubix/Ping/VerifyStakedToken.java
+++ src/com/rubix/Ping/VerifyStakedToken.java
@@ -1,20 +1,27 @@
 package com.rubix.Ping;
 
-import io.ipfs.api.IPFS;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
-import org.json.JSONException;
-import org.json.JSONObject;
+import static com.rubix.Resources.Functions.IPFS_PORT;
+import static com.rubix.Resources.Functions.LOGGER_PATH;
+import static com.rubix.Resources.Functions.SYNC_IP;
+import static com.rubix.Resources.IPFSNetwork.executeIPFSCommands;
+import static com.rubix.Resources.IPFSNetwork.forward;
+import static com.rubix.Resources.IPFSNetwork.repo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.URL;
 
-import static com.rubix.Resources.Functions.*;
-import static com.rubix.Resources.IPFSNetwork.*;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.ipfs.api.IPFS;
 
 public class VerifyStakedToken {
     private static final Logger PingSenderLogger = Logger.getLogger(VerifyStakedToken.class);
@@ -51,7 +58,6 @@ public class VerifyStakedToken {
             return false;
         }
 
-
         int height = 0;
         if (heightResponse == null) {
             executeIPFSCommands(" ipfs p2p close -t /p2p/" + pid);
@@ -62,7 +68,7 @@ public class VerifyStakedToken {
             APIResponse.put("status", "Failed");
             APIResponse.put("message", "TokenChain height not received");
 
-        }else {
+        } else {
             PingSenderLogger.info("TokenChain height received from " + pid);
             PingSenderLogger.info("TokenChain height: " + heightResponse);
             executeIPFSCommands(" ipfs p2p close -t /p2p/" + pid);
@@ -72,9 +78,41 @@ public class VerifyStakedToken {
             APIResponse.put("status", "Success");
             APIResponse.put("message", Integer.parseInt(heightResponse));
             height = Integer.parseInt(heightResponse);
-
         }
 
-        return height >= 46;
+        String tokenLevel = token.substring(0, 3);
+        int tokenLevelInt = Integer.parseInt(tokenLevel);
+        int tokenLevelValue = (int) Math.pow(2, tokenLevelInt + 2);
+        int requiredMinedTokenHeight = tokenLevelValue * 4;
+
+        String GET_URL_credit = SYNC_IP + "/getCurrentLevel";
+        URL URLobj_credit = new URL(GET_URL_credit);
+        HttpURLConnection con_credit = (HttpURLConnection) URLobj_credit.openConnection();
+        con_credit.setRequestMethod("GET");
+        int responseCode_credit = con_credit.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode_credit);
+        if (responseCode_credit == HttpURLConnection.HTTP_OK) {
+            BufferedReader in_credit = new BufferedReader(
+                    new InputStreamReader(con_credit.getInputStream()));
+            String inputLine_credit;
+            StringBuffer response_credit = new StringBuffer();
+            while ((inputLine_credit = in_credit.readLine()) != null) {
+                response_credit.append(inputLine_credit);
+            }
+            in_credit.close();
+            // QuorumConsensusLogger.debug("response from service " +
+            // response_credit.toString());
+            JSONObject resJsonData_credit = new JSONObject(response_credit.toString());
+            int level_credit = resJsonData_credit.getInt("level");
+
+            // ! release staked token if the mined token is from previous level(s)
+            if (level_credit > tokenLevelInt) {
+                return true;
+            }
+
+        } else
+            PingSenderLogger.debug("GET request not worked");
+
+        return height > requiredMinedTokenHeight;
     }
 }
