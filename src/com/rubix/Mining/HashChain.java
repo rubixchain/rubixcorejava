01be836c4f17742867cf7afd814bb0f221d06e6c
Gokul P S <94040889+gklps@users.noreply.github.com>
2022-04-28 11:43:33 +0530

    Add files via upload

---
+++ src/com/rubix/Mining/HashChain.java
@@ -0,0 +1,49 @@
+package com.rubix.Mining;
+
+import com.rubix.Resources.Functions;
+
+import org.apache.log4j.Logger;
+
+public class HashChain {
+
+    public static Logger HashChainLogger = Logger.getLogger(HashChain.class);
+
+    private static String finalHash = "";
+    private static int iterCount = 0;
+
+    public static String newHashChain(String miningTID, String[] DIDs) {
+
+        finalHash = miningTID;
+
+        do {
+
+            iterCount++;
+            finalHash = Functions.calculateHash(finalHash, "SHA3-256");
+
+        } while (ruleNotMatch(DIDs));
+
+        HashChainLogger.trace("Hash Chain Length for TID: " + miningTID + " is = " + iterCount);
+        return finalHash;
+    }
+
+    public static Boolean verifyHashChain(String tokenTID, String finalHash, String[] DIDs) {
+
+        String calculatedFinalHash = newHashChain(tokenTID, DIDs);
+
+        return calculatedFinalHash == finalHash;
+    }
+
+    private static Boolean ruleNotMatch(String[] DIDs) {
+        HashChainLogger.trace("Hash Chain " + iterCount + " > " + finalHash);
+        int MATCH_RULE = 1;
+
+        for (int i = 0; i < DIDs.length; i++) {
+            if (finalHash.substring(finalHash.length() - MATCH_RULE)
+                    .equals(DIDs[i].substring(DIDs[i].length() - MATCH_RULE))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
