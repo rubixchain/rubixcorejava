8d78bf0c29110090889f015a0021ad6d0079f054
Nidhin Mahesh A <nidhin.m@rubix.network>
2022-05-10 12:16:04 +0530

    averaging puzzle
    Signed-off-by: Nidhin Mahesh A <nidhin.m@rubix.network>

--- src/com/rubix/Mining/HashChain.java
+++ src/com/rubix/Mining/HashChain.java
@@ -8,72 +8,72 @@ public class HashChain {
 
     public static Logger HashChainLogger = Logger.getLogger(HashChain.class);
 
+    // write a function which takes a string as TID and string array as DIDs. The
+    // function should return the average iterations each DID took to rehash itself
+    // to match the last 3 char of TID
+    public static String newHashChain(String TID, String[] DIDs) {
+        int[] averageIterations = new int[DIDs.length];
+        for (int i = 0; i < DIDs.length; i++) {
+            averageIterations[i] = averageIterations(TID, DIDs[i]);
+        }
+        // get the average of values in the array
+        int sum = 0;
+        for (int i = 0; i < averageIterations.length; i++) {
+            System.out.println("averageIterations[" + i + "] = " + averageIterations[i]);
+            sum += averageIterations[i];
+        }
+        int average = sum / averageIterations.length;
+        return Integer.toString(average);
+    }
+
+    private static int averageIterations(String TID, String DID) {
+        int iterations = 0;
+        int matchRule = 5;
+        String last3Chars = TID.substring(TID.length() - matchRule);
+        String currentDID = DID;
+        while (!currentDID.substring(currentDID.length() - matchRule).equals(last3Chars)) {
+            currentDID = Functions.calculateHash(currentDID, "SHA3-256");
+            iterations++;
+
+            System.out.println(currentDID + " " + iterations);
+        }
+        return iterations;
+    }
+
+    // write a function which takes a string as TID and string array as DIDs. TID
+    // will hash itself until last 5 characters are same as any one of the DIDs.
+    // If not found, return null.
+    // public static String newHashChain(String TID, String[] DIDs) {
+    // String TIDHash = TID;
+
+    // while (!Functions.isSame(TIDHash.substring(TIDHash.length() - 5), DIDs)) {
+    // TIDHash = Functions.calculateHash(TIDHash, "SHA3-256");
+    // }
+
+    // return TIDHash;
+    // }
+
     // write a function which takes a string as TID and string array as DIDs. DIDs
     // will hash itself continuously and return when the last 3 char of hash matches
     // last 3 char of TID
-    public static String newHashChain(String tID, String[] DIDs) {
-        String hashChain = "";
-        int counter = 0;
-        for (String DID : DIDs) {
+    // public static String newHashChain(String tID, String[] DIDs) {
+    // String hashChain = "";
+    // int counter = 0;
+    // for (String DID : DIDs) {
 
-            String hash = DID;
-            while (!hash.endsWith(tID.substring(tID.length() - DIDs.length))) {
-                // hashChain += hash;
-                counter++;
-                hash = Functions.calculateHash(hash, "SHA3-256");
-                hashChain = hash;
-                System.out.println(hash + " " + counter);
-            }
-
-        }
-
-        return hashChain;
-
-    }
-    // private static String finalHash = "";
-    // private static int iterCount = 0;
-
-    // public static String newHashChain(String miningTID, String[] DIDs) {
-
-    // finalHash = miningTID;
-
-    // if (finalHash != null && DIDs.length > 0) {
-
-    // while (ruleNotMatch(DIDs)) {
-
-    // iterCount++;
-    // finalHash = Functions.calculateHash(finalHash, "SHA3-256");
-    // System.out.println("HashChain at " + iterCount + " is " + finalHash);
+    // String hash = DID;
+    // while (!hash.endsWith(tID.substring(tID.length() - DIDs.length))) {
+    // // hashChain += hash;
+    // counter++;
+    // hash = Functions.calculateHash(hash, "SHA3-256");
+    // hashChain = hash;
+    // System.out.println(hash + " " + counter);
+    // }
 
     // }
 
-    // HashChainLogger.debug("Hash Chain Length for TID: " + miningTID + " is = " +
-    // iterCount);
-    // }
-    // return finalHash;
-    // }
+    // return hashChain;
 
-    // public static Boolean verifyHashChain(String tokenTID, String challengeHash,
-    // String[] DIDs) {
-
-    // String calculatedFinalHash = newHashChain(tokenTID, DIDs);
-
-    // return calculatedFinalHash == challengeHash;
-    // }
-
-    // private static Boolean ruleNotMatch(String[] DIDs) {
-    // int matchRule = 5;
-
-    // for (int i = 0; i < 3; i++) {
-    // if (finalHash.substring(finalHash.length() - matchRule)
-    // .equals(DIDs[i].substring(DIDs[i].length() - matchRule))) {
-    // System.out.println("Rule " + i + " matched");
-    // return false;
-    // }
-    // }
-
-    // System.out.println("Rule not matched");
-    // return true;
     // }
 
 }
