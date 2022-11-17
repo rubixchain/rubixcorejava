d2ce2919ef1dba5ba89f0d7975869e26b60b7054
Kiran H <kiran.h@rubix.network>
2022-11-17 15:34:17 +0530

    added logs

--- src/com/rubix/TokenTransfer/TransferPledge/Initiator.java
+++ src/com/rubix/TokenTransfer/TransferPledge/Initiator.java
@@ -10,6 +10,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
@@ -279,6 +280,10 @@ public class Initiator {
 //                            String hashForTokenChain = calculateHash(tokenChain.toString(), "SHA3-256");
 							String signString = "";
 							String hashString = "";
+
+							FileWriter spfile = new FileWriter(WALLET_DATA_PATH.concat("/partBPlegeTest").concat(tid).concat(".json"));
+                            spfile.write(pledgeDetails.toString());
+                            spfile.close();
 
 							for (int i = 0; i < pledgeDetails.length(); i++) {
 								PledgeInitiatorLogger.debug("pledgeDetails is " + pledgeDetails.getJSONObject(i));
